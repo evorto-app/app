98930a9d5c3b399c838a75797ff117f210be0540
Lukas heddendorp <lu.heddendorp@gmail.com>
2025-05-30 17:45:43 +0200

    @angular/cli migration - replace-provide-server-routing
    Migrate 'provideServerRendering' to use 'withRoutes', and remove 'provideServerRouting' and 'provideServerRoutesConfig' from '@angular/ssr'.

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,11 @@
-import { provideServerRendering, provideServerRouting } from '@angular/ssr';
+import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(), provideServerRouting(serverRoutes)],
+  providers: [provideServerRendering(withRoutes(serverRoutes))],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
