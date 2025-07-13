c36274c616a916918c74725cc599e1035316101c
Lukas heddendorp <lu.heddendorp@gmail.com>
2025-07-13 19:34:12 +0200

    Example use of the new trpc integration

--- src/app/internal-pages/members-hub/members-hub.component.ts
+++ src/app/internal-pages/members-hub/members-hub.component.ts
@@ -1,21 +1,19 @@
+import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { injectTRPC } from '@heddendorp/tanstack-angular-query';
 import { injectQuery } from '@tanstack/angular-query-experimental';
 
-import { AppRouter } from '../../../server/trpc/app-router';
-
-// import { injectTRPC } from '../../core/trpc-client';
+import { injectTRPC } from '../../core/trpc-client';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [JsonPipe],
   selector: 'app-members-hub',
   styles: ``,
   templateUrl: './members-hub.component.html',
 })
 export class MembersHubComponent {
-  private readonly trpc = injectTRPC<AppRouter>();
-  protected readonly rolesQuery = injectQuery(
+  private readonly trpc = injectTRPC();
+  protected readonly rolesQuery = injectQuery(() =>
     this.trpc.admin.roles.findMany.queryOptions({}),
   );
 }
