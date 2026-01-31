68594b599c6a105d47da4038ec764babdff4e333
Lukas heddendorp <lu.heddendorp@gmail.com>
2026-01-31 18:05:28 +0100

    chore(playwright): point tests to root tests directory

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,13 +43,14 @@ export default defineConfig({
   projects: [
     {
       name: 'setup',
+      testDir: './e2e/setup',
       testMatch: /.*\.setup\.ts$/,
       use: { ...devices['Desktop Chrome'] },
     },
     {
       dependencies: ['setup'],
       name: 'docs',
-      testMatch: /.*\.doc\.ts$/,
+      testMatch: /docs\/.*\.doc\.ts$/,
       timeout: 60 * 1000,
       use: { ...devices['Desktop Chrome'], channel: 'chromium' },
     },
@@ -102,7 +103,7 @@ export default defineConfig({
   reporter: reporters,
   /* Retry on CI only */
   retries: process.env['CI'] ? 2 : 0,
-  testDir: './e2e',
+  testDir: './tests',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
