1dd9751042e3b22a57587b5db9f925cff9bd99ec
Lukas heddendorp <lu.heddendorp@gmail.com>
2026-02-13 12:58:31 +0100

    refactor(trpc): remove unused users and global-admin routers

--- src/server/trpc/app-router.ts
+++ src/server/trpc/app-router.ts
@@ -1,11 +1,7 @@
 import { financeRouter } from './finance/finance.router';
-import { globalAdminRouter } from './global-admin/global-admin.router';
 import { router } from './trpc-server';
-import { userRouter } from './users/users.router';
 
 export const appRouter = router({
   finance: financeRouter,
-  globalAdmin: globalAdminRouter,
-  users: userRouter,
 });
 export type AppRouter = typeof appRouter;
